aeaba87ebc4c765516990e871f96d26c4563bdb3
TumeloFRakgwahla <tumelorakgwahla@gmail.com>
2025-06-25 18:35:26 +0200

    Integrated vue into laravel and created the Home page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import "./bootstrap";
 import { createApp } from "vue";
+import vuetify from "./vuetify";
 import app from "./layouts/app.vue";
  
-createApp(app).mount("#app");
+createApp(app).use(vuetify).mount("#app");
